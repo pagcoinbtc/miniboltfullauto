ff2c2e62534659403d8ce6c32104fe8db8aa0c58
pagcoinbtc <167659900+pagcoinbtc@users.noreply.github.com>
2024-08-06 11:26:46 -0300

    Create create_lndservice.sh

---
+++ create_lndservice.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Cria o arquivo de serviço systemd para o lnd
+sudo bash -c 'cat << EOF > /etc/systemd/system/lnd.service
+# MiniBolt: systemd unit for lnd
+# /etc/systemd/system/lnd.service
+
+[Unit]
+Description=Lightning Network Daemon
+
+[Service]
+ExecStart=/usr/local/bin/lnd
+ExecStop=/usr/local/bin/lncli stop
+
+# Process management
+####################
+Restart=on-failure
+RestartSec=60
+Type=notify
+TimeoutStartSec=1200
+TimeoutStopSec=3600
+
+# Directory creation and permissions
+####################################
+RuntimeDirectory=lightningd
+RuntimeDirectoryMode=0710
+User=lnd
+Group=lnd
+
+# Hardening Measures
+####################
+PrivateTmp=true
+ProtectSystem=full
+NoNewPrivileges=true
+PrivateDevices=true
+MemoryDenyWriteExecute=true
+
+[Install]
+WantedBy=multi-user.target
+EOF'
+
+# Habilita e inicia o serviço lnd
+sudo systemctl enable lnd
+sudo systemctl start lnd
