cfa4e8c8cedbd280cd0f591250438bee7b92d129
pagcoinbtc <167659900+pagcoinbtc@users.noreply.github.com>
2024-08-10 16:04:57 -0300

    Create bos-autostart.sh

---
+++ bos-autostart.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Solicita o código de conexão do Telegram
+read -p "Telegram Connection code: " telegram_code
+
+# Cria o arquivo bos-telegram.service em /etc/systemd/system/
+sudo bash -c "cat <<EOF > /etc/systemd/system/bos-telegram.service
+# Systemd unit for Bos-Telegram Bot
+# /etc/systemd/system/bos-telegram.service
+# Substitua as variáveis iniciadas com \$ com suas informações
+# Não esquece de apagar o \$
+
+[Unit]
+Description=bos-telegram
+Wants=lnd.service
+After=lnd.service
+
+[Service]
+ExecStart=/home/admin/.npm-global/bin/bos telegram --use-small-units --connect $telegram_code
+User=admin
+Restart=always
+TimeoutSec=120
+RestartSec=30
+StandardOutput=null
+StandardError=journal
+Environment=BOS_DEFAULT_LND_PATH=/data/lnd
+
+[Install]
+WantedBy=multi-user.target
+EOF"
+
+# Atualiza o daemon do systemd
+sudo systemctl daemon-reload
+
+# Inicia o serviço bos-telegram
+sudo systemctl start bos-telegram.service
+
+# Habilita o serviço para iniciar com o sistema
+sudo systemctl enable bos-telegram.service
+
+# Mostra o status do serviço
+sudo systemctl status bos-telegram.service
