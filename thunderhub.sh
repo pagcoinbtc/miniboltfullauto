1f102354afbd6b523b8acb57627c8604e75454e7
pagcoinbtc <167659900+pagcoinbtc@users.noreply.github.com>
2024-08-11 02:07:52 -0300

    Update thunderhub.sh

--- thunderhub.sh
+++ thunderhub.sh
@@ -1,7 +1,33 @@
-# install service
-    echo "*** Install ThundeHub systemd for ${network} on ${chain} ***"
-    cat > /home/admin/thunderhub.service <<EOF
-# Systemd unit for thunderhub
+#!/bin/bash
+
+# Variáveis de configuração
+THUNDERHUB_PORT=3010
+THUNDERHUB_DIR=/home/admin/thunderhub
+NODE_NETWORK=mainnet # ou testnet, se aplicável
+NODE_CHAIN=bitcoin   # ou litecoin, se aplicável
+
+# Verifique se o Node.js está instalado e atualizado
+curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# Configure o firewall para permitir o acesso ao ThunderHub
+sudo ufw allow from 192.168.0.0/16 to any port $THUNDERHUB_PORT comment 'allow ThunderHub on LAN'
+
+# Se o Tor estiver ativado, crie um serviço oculto para o ThunderHub
+/home/admin/config.scripts/internet.hiddenservice.sh thunderhub 80 $THUNDERHUB_PORT
+
+# Clone o repositório do ThunderHub e instale as dependências
+git clone https://github.com/apotdevin/thunderhub.git $THUNDERHUB_DIR
+cd $THUNDERHUB_DIR
+npm install --force
+
+# Execute a build do ThunderHub
+npm run build
+
+# Crie o serviço systemd para o ThunderHub
+echo "*** Install ThunderHub systemd for ${NODE_NETWORK} on ${NODE_CHAIN} ***"
+sudo bash -c "cat > /etc/systemd/system/thunderhub.service <<EOF
+# Systemd unit for ThunderHub
 # /etc/systemd/system/thunderhub.service
 
 [Unit]
@@ -10,8 +36,8 @@ Wants=lnd.service
 After=lnd.service
 
 [Service]
-WorkingDirectory=/home/admin/thunderhub
-ExecStart=/usr/bin/npm run start -- -p 3010
+WorkingDirectory=$THUNDERHUB_DIR
+ExecStart=/usr/bin/npm run start -- -p $THUNDERHUB_PORT
 User=admin
 Restart=always
 TimeoutSec=120
@@ -21,11 +47,12 @@ StandardError=journal
 
 [Install]
 WantedBy=multi-user.target
-EOF
+EOF"
 
-    sudo mv /home/admin/thunderhub.service /etc/systemd/system/thunderhub.service 
-    sudo chown root:root /etc/systemd/system/thunderhub.service
-    sudo systemctl enable thunderhub
-    echo "OK - the ThunderHub service is now enabled"
- 
- sudo systemctl start thunderhub
+# Ajuste as permissões e habilite o serviço
+sudo chown root:root /etc/systemd/system/thunderhub.service
+sudo systemctl enable thunderhub
+
+# Inicie o serviço e verifique o status
+sudo systemctl start thunderhub
+sudo systemctl status thunderhub
