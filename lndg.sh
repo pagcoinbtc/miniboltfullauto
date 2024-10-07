e1cf9acc0ec8e38c9d4c1598ca284b55c6545061
pagcoinbtc <167659900+pagcoinbtc@users.noreply.github.com>
2024-10-07 18:05:26 -0300

    Update lndg.sh

--- lndg.sh
+++ lndg.sh
@@ -9,6 +9,9 @@ GITHUB_CRIPTOSHARK=https://github.com/cryptosharks131/lndg.git
 # Atualiza os pacotes do sistema
 sudo apt update && sudo apt full-upgrade -y
 
+# Volta a home
+cd
+
 # Clona o repositório
 git clone $GITHUB_CRIPTOSHARK && cd lndg
 
@@ -30,22 +33,47 @@ virtualenv -p python3 .venv
 # Usa um script para o backend
 sudo bash systemd.sh
 
-# Cria o service para o frontend
-sudo cat <<EOF > /etc/systemd/system/uwsgi.service
+# Cria o service para o backend
+sudo cat <<EOF > /etc/systemd/system/lndg-controller.service
 [Unit]
-Description=Lndg uWSGI app
-After=syslog.target
+Description=Controlador de backend para Lndg
 
 [Service]
-ExecStart=/home/admin/lndg/.venv/bin/python /home/admin/lndg/manage.py runserver 0.0.0.0:8889
+Environment=PYTHONUNBUFFERED=1
 User=admin
-Group=www-data
-Restart=on-failure
-KillSignal=SIGQUIT
-Type=notify
-StandardError=syslog
-NotifyAccess=all
-Environment="PATH=/home/admin/lndg/.venv/bin"
+Group=admin
+ExecStart=/home/admin/lndg/.venv/bin/python /home/admin/lndg/controller.py
+StandardOutput=append:/var/log/lndg-controller.log
+StandardError=append:/var/log/lndg-controller.log
+Restart=always
+RestartSec=60s
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Cria o service para o frontend
+sudo cat <<EOF > /etc/systemd/system/lndg.service
+[Unit]
+Description=LNDG Django Server
+After=network.target
+
+# Recarrega o systemd
+sudo systemctl daemon-reload
+sudo systemctl restart lndg-controller.service
+sudo systemctl status lndg-controller.service
+
+[Service]
+Environment=PYTHONUNBUFFERED=1
+User=admin
+Group=admin
+WorkingDirectory=/home/admin/lndg
+ExecStart=/home/admin/lndg/.venv/bin/python /home/admin/lndg/manage.py runserver 0.0.0.0:8889
+StandardOutput=append:/var/log/lndg.log
+StandardError=append:/var/log/lndg.log
+Restart=always
+RestartSec=5
+TimeoutSec=300
 
 [Install]
 WantedBy=multi-user.target
@@ -53,4 +81,5 @@ EOF
 
 # Recarrega o systemd
 sudo systemctl daemon-reload
-sudo systemctl status uwsgi.service
+sudo systemctl restart lndg.service
+sudo systemctl status lndg.service
