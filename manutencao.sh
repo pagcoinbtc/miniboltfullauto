17b2ace06d108b8a682bcbf6c149f37cb1b023c1
pagcoinbtc <167659900+pagcoinbtc@users.noreply.github.com>
2024-10-19 01:34:05 -0700

    Update manutencao.sh

--- manutencao.sh
+++ manutencao.sh
@@ -62,6 +62,36 @@ thunderhub_update () {
   head -n 3 /home/thunderhub/thunderhub/package.json | grep version
 {
 
+thunderhub_uninstall () {
+  sudo systemctl stop thunderhub
+  sudo systemctl disable thunderhub
+  sudo rm -rf /home/thunderhub/thunderhub
+  sudo rm -rf /etc/systemd/system/thunderhub.service
+  sudo rm -rf /etc/nginx/sites-available/thunderhub-reverse-proxy.conf
+  echo "Thunderhub desinstalado!"
+}
+
+lndg_unninstall () {
+  sudo systemctl stop lndg.service
+  sudo systemctl disable lndg.service
+  sudo systemctl stop lndg-controller.service
+  sudo systemctl disable lndg-controller.service
+  sudo rm -rf /home/admin/lndg
+  sudo rm -rf /etc/systemd/system/lndg.service
+  sudo rm -rf /etc/systemd/system/lndg-controller.service
+  sudo rm -rf /etc/nginx/sites-available/lndg-reverse-proxy.conf
+  echo "LNDg desinstalado!"
+}
+
+lnbits_unninstall () {
+  sudo systemctl stop lnbits
+  sudo systemctl disable lnbits
+  sudo rm -rf /home/admin/lnbits
+  sudo rm -rf /etc/systemd/system/lnbits.service
+  sudo rm -rf /etc/nginx/sites-available/lnbits-reverse-proxy.conf
+  echo "LNbits desinstalado!"
+}
+
 pacotes_do_sistema () {
   sudo apt update && sudo apt upgrade -y
   sudo systemctl reload tor
@@ -75,6 +105,9 @@ menu() {
   echo "Antes de atualizar o Bitcoind, leia as notas de atualização"
   echo "3) Atualizar o Thunderhub"
   echo "4) Atualizar os pacotes do sistema"
+  echo "5) Desinstalar Thunderhub"
+  echo "6) Desinstalar LNDg"
+  echo "7) Desinstalar LNbits"
   echo "0) Sair"
   read -p "Opção: " option
 
@@ -90,6 +123,15 @@ menu() {
       ;;
     4)
       pacotes_do_sistema
+      ;;
+    5)Desinstalar Thunderhub
+      thunderhub_uninstall
+      ;; 
+    6)Desinstalar LNDg
+      lndg_unninstall
+      ;;
+    7)Desinstalar LNbits
+      lnbits_unninstall
       ;;
     0)
       echo "Saindo..."
