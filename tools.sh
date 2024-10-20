f74ba939fde744552e7b56ca29ca8ce9c8940471
pagcoinbtc <167659900+pagcoinbtc@users.noreply.github.com>
2024-10-20 00:19:05 -0700

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -7,66 +7,44 @@
 # Define as variáveis
 LNDG_DIR=/home/admin/lndg
 VERSION_THUB=0.13.31
-read -sp "Digite a senha para ThunderHub: " senha
-read -p "Digite o alias do seu node: " nome_do_seu_node
 
-# Atualiza os pacotes do sistema
-sudo apt update && sudo apt full-upgrade -y
+system_update() {
+  sudo apt update && sudo apt full-upgrade -y
+}
 
-# Atualiza e instala o Node.js
-curl -sL https://deb.nodesource.com/setup_21.x | sudo -E bash -
-sudo apt-get install nodejs -y
+install_nodejs() {
+  curl -sL https://deb.nodesource.com/setup_21.x | sudo -E bash -
+  sudo apt-get install nodejs -y
+  
+}
 
-# Inicio da instalação do bos (Balance os Satoshis)
-# Configura npm para instalação do bos global sem sudo
-mkdir -p ~/.npm-global
-npm config set prefix '~/.npm-global'
-
-# Adiciona o caminho do npm global ao PATH no ~/.profile
+install_bos() {
+  read -p "Digite o alias do seu node: " nome_do_seu_node
+  mkdir -p ~/.npm-global
+  npm config set prefix '~/.npm-global'
 if ! grep -q 'PATH="$HOME/.npm-global/bin:$PATH"' ~/.profile; then
   echo 'PATH="$HOME/.npm-global/bin:$PATH"' >> ~/.profile
 fi
-
-# Carrega o perfil atualizado
-source ~/.profile
-
-# Instala o Balance of Satoshis (bos)
-npm i -g balanceofsatoshis
-
-# Verifica a instalação
-bos --version
-
-# Atualiza o arquivo /etc/hosts
-sudo bash -c 'echo "127.0.0.1" >> /etc/hosts'
-
-# Ajusta permissões do diretório LND
-sudo chown -R $USER:$USER /data/lnd
-sudo chmod -R 755 /data/lnd
-
-# Exporta a variável de ambiente BOS_DEFAULT_LND_PATH
-export BOS_DEFAULT_LND_PATH=/data/lnd
-
-# Cria o diretório para o node do bos
-mkdir -p ~/.bos/$nome_do_seu_node
-
-# Gera os arquivos base64 do certificado TLS e do macaroon admin
-base64 -w0 /data/lnd/tls.cert > /data/lnd/tls.cert.base64
-base64 -w0 /data/lnd/data/chain/bitcoin/mainnet/admin.macaroon > /data/lnd/data/chain/bitcoin/mainnet/admin.macaroon.base64
-
-# Cria o arquivo credentials.json com os valores codificados em base64
-cert_base64=$(cat /data/lnd/tls.cert.base64)
-macaroon_base64=$(cat /data/lnd/data/chain/bitcoin/mainnet/admin.macaroon.base64)
-
-bash -c "cat <<EOF > ~/.bos/$nome_do_seu_node/credentials.json
+  source ~/.profile
+  npm i -g balanceofsatoshis
+  bos --version
+  sudo bash -c 'echo "127.0.0.1" >> /etc/hosts'
+  sudo chown -R $USER:$USER /data/lnd
+  sudo chmod -R 755 /data/lnd
+  export BOS_DEFAULT_LND_PATH=/data/lnd
+  mkdir -p ~/.bos/$nome_do_seu_node
+  base64 -w0 /data/lnd/tls.cert > /data/lnd/tls.cert.base64
+  base64 -w0 /data/lnd/data/chain/bitcoin/mainnet/admin.macaroon > /data/lnd/data/chain/bitcoin/mainnet/admin.macaroon.base64
+  cert_base64=$(cat /data/lnd/tls.cert.base64)
+  macaroon_base64=$(cat /data/lnd/data/chain/bitcoin/mainnet/admin.macaroon.base64)
+  bash -c "cat <<EOF > ~/.bos/$nome_do_seu_node/credentials.json
 {
   "cert": "$cert_base64",
   "macaroon": "$macaroon_base64",
   "socket": "localhost:10009"
 }
 EOF"
-
-# Cria o arquivo bos-telegram.service em /etc/systemd/system/
-sudo bash -c "cat <<EOF > /etc/systemd/system/bos-telegram.service
+  sudo bash -c "cat <<EOF > /etc/systemd/system/bos-telegram.service
 # Systemd unit for Bos-Telegram Bot
 # /etc/systemd/system/bos-telegram.service
 # Substitua as variáveis iniciadas com \$ com suas informações
@@ -90,47 +68,24 @@ Environment=BOS_DEFAULT_LND_PATH=/data/lnd
 [Install]
 WantedBy=multi-user.target
 EOF"
+  sudo systemctl daemon-reload
+  sudo systemctl start bos-telegram.service
+  sudo systemctl enable bos-telegram.service
+}
 
-# Atualiza o daemon do systemd
-sudo systemctl daemon-reload
-
-# Inicia o serviço bos-telegram
-sudo systemctl start bos-telegram.service
-
-# Habilita o serviço para iniciar com o sistema
-sudo systemctl enable bos-telegram.service
-
-# Atualiza os pacotes do sistema
-sudo apt update && sudo apt full-upgrade -y
-
-# Verifica se os pré-requisitos estão instalados
-node -v
-npm -v
-
-# Volta ao diretório home
-cd
-
-## Inicia a instalação do thunderhub
-# Importa a chave GPG do repositório do ThunderHub
-curl https://github.com/apotdevin.gpg | gpg --import
-
-# Clona o repositório do ThunderHub na versão especificada e entra no diretório
-git clone --branch v$VERSION_THUB https://github.com/apotdevin/thunderhub.git && cd thunderhub
-
-# Verifica a integridade do commit
-git verify-commit v$VERSION_THUB
-
-# Atualiza os pacotes do sistema
-sudo apt update && sudo apt full-upgrade -y
-
-# Instala as dependências do ThunderHub
-npm install
-
-# Executa a build do ThunderHub
-npm run build
-
-# Configura o nginx
-sudo tee /etc/nginx/sites-available/thunderhub-reverse-proxy.conf > /dev/null  <<EOF
+install_thunderhub() {
+  node -v
+  npm -v
+  read -p "Digite a senha para ThunderHub: " senha
+  sudo apt update && sudo apt full-upgrade -y
+  cd
+  curl https://github.com/apotdevin.gpg | gpg --import
+  git clone --branch v$VERSION_THUB https://github.com/apotdevin/thunderhub.git && cd thunderhub
+  git verify-commit v$VERSION_THUB
+  sudo apt update && sudo apt full-upgrade -y
+  npm install
+  npm run build
+  sudo tee /etc/nginx/sites-available/thunderhub-reverse-proxy.conf > /dev/null  <<EOF
 server {
   listen 4002;
   location / {
@@ -140,18 +95,9 @@ server {
 EOF
 sudo ln -s /etc/nginx/sites-available/thunderhub-reverse-proxy.conf /etc/nginx/sites-enabled/
 sudo nginx -t
-sudo systemctl reload nginx
 sudo ufw allow 4002/tcp comment 'allow ThunderHub SSL from anywhere'
-
-# Copiar o modelo do ficheiro de configuração
 cp .env .env.local
-
-# Editar a linha 51 do arquivo .env.local para incluir a configuração desejada
-# Usando sed para substituir a linha 51
 sed -i '51s|.*|ACCOUNT_CONFIG_PATH="/home/admin/thunderhub/thubConfig.yaml"|' .env.local
-
-# Cria a configuração da conta
-# Criar ou sobrescrever o arquivo thubConfig.yaml com o conteúdo inicial
 bash -c "cat <<EOF > thubConfig.yaml
 masterPassword: 'PASSWORD'
 accounts:
@@ -161,12 +107,7 @@ accounts:
     certificatePath: '/data/lnd/tls.cert'
     password: '[E] ThunderHub password'
 EOF"
-
-# Usar sed para substituir a senha na linha 7
-# Procurar pela palavra "[E] ThunderHub password" e substituir pela senha fornecida
 sed -i "7s|\[E\] ThunderHub password|$senha|" thubConfig.yaml
-
-# Cria o serviço systemd para o ThunderHub
 sudo bash -c 'cat <<EOF > /etc/systemd/system/thunderhub.service
 # MiniBolt: systemd unit for Thunderhub
 # /etc/systemd/system/thunderhub.service
@@ -196,32 +137,21 @@ PrivateDevices=true
 [Install]
 WantedBy=multi-user.target
 EOF'
-
-# Habilita o serviço para iniciar com o sistema e o inicia
 sudo systemctl enable thunderhub
 sudo systemctl start thunderhub
 sudo systemctl reload nginx
+}
 
-## Inicio da intalação do LNDG
-# Configura o firewall no lndg
+install_lndg () {
 sudo ufw allow 8889/tcp comment 'allow lndg SSL from anywhere'
-
-# Volta à home
 cd
 git clone https://github.com/cryptosharks131/lndg.git
 cd lndg
-sudo apt install virtualenv
+sudo apt install -y virtualenv
 virtualenv -p python3 .venv
 .venv/bin/pip install -r requirements.txt
 .venv/bin/pip install whitenoise
-.venv/bin/python initialize.py -wn
-echo 'SUA SENHA DO LNDG ESTÁ SENDO CRIADA ISTO PODE DEMORAR ALGUNS MINUTOS
-###APÓS A INSTALAÇÃO VENHA E COPIE A SENHA PARA ACESSAR O LNDG###'
-
-# Executa o script de inicialização e captura a senha gerada
 .venv/bin/python initialize.py --whitenoise
-
-# Cria o service para o backend
 sudo tee /etc/systemd/system/lndg-controller.service > /dev/null <<EOF
 [Unit]
 Description=Controlador de backend para Lndg
@@ -239,8 +169,6 @@ RestartSec=60s
 [Install]
 WantedBy=multi-user.target
 EOF
-
-# Cria o service para o frontend
 sudo tee /etc/systemd/system/lndg.service > /dev/null  <<EOF
 [Unit]
 Description=LNDG Django Server
@@ -261,8 +189,14 @@ TimeoutSec=300
 [Install]
 WantedBy=multi-user.target
 EOF
+sudo systemctl daemon-reload
+sudo systemctl enable lndg-controller.service
+sudo systemctl start lndg-controller.service
+sudo systemctl enable lndg.service
+sudo systemctl start lndg.service
+}
 
-# Cria o serviço do lnbits
+install_lnbits() {
 sudo bash -c 'cat <<EOF > /etc/systemd/system/lnbits.service
 [Unit]
 Description=LNbits Service
@@ -277,18 +211,7 @@ Restart=always
 [Install]
 WantedBy=multi-user.target
 EOF'
-
-# Recarrega e reinicia os serviços
-sudo systemctl daemon-reload
-sudo systemctl enable lndg-controller.service
-sudo systemctl start lndg-controller.service
-sudo systemctl enable lndg.service
-sudo systemctl start lndg.service
-
-# Volta ao diretório home
 cd 
-
-# Configura o nginx do lnbits
 sudo bash -c "cat <<EOF > /etc/nginx/sites-available/lnbits-reverse-proxy.conf
 server {
   listen 4003;
@@ -297,38 +220,48 @@ server {
   }
 }
 EOF"
-
 sudo ln -s /etc/nginx/sites-available/lnbits-reverse-proxy.conf /etc/nginx/sites-enabled/
 sudo nginx -t
 sudo systemctl reload nginx
 sudo ufw allow 4003/tcp comment 'allow lnbits SSL from anywhere'
-
-## Instalação do lnbits por script
-# instala o poetry
-sudo apt install python3-poetry &&
-export PATH="/home/$USER/.local/bin:$PATH"
-sudo apt update -y
-sudo apt install -y software-properties-common
-
-# Clone the LNbits repository
-git clone https://github.com/lnbits/lnbits.git
-cd lnbits
-git checkout main
-poetry install --only main
-
-# Copy the .env.example to .env
-cp .env.example .env
-
-cd lnbits
-# Install the dependencies using Poetry
+if [ ! -d lnbits/data ]; then
+  sudo apt update -y
+  sudo apt install -y software-properties-common
+  sudo add-apt-repository -y ppa:deadsnakes/ppa
+  sudo apt install -y python3.9 python3.9-distutils
+  curl -sSL https://install.python-poetry.org | python3.9 -
+  export PATH="/home/$USER/.local/bin:$PATH"
+  if [ ! -d lnbits/wallets ]; then
+    git clone https://github.com/lnbits/lnbits.git
+    if [ $? -ne 0 ]; then
+      echo "Failed to clone the repository ... FAIL"
+      exit 1
+    fi
+    cd lnbits || { echo "Failed to cd into lnbits ... FAIL"; exit 1; }
+  fi
+  git checkout main
+  mkdir data
+  cp .env.example .env
+elif [ ! -d lnbits/wallets ]; then
+  # cd into lnbits
+  cd lnbits || { echo "Failed to cd into lnbits ... FAIL"; exit 1; }
+fi
 poetry env use python3.9
 poetry install --only main
-
-# Set environment variables for LNbits
 export LNBITS_ADMIN_UI=true
 export HOST=0.0.0.0
-
-# Inicia o serviço do lnbits
 sudo systemctl enable lnbits.service
 sudo systemctl start lnbits.service
 echo "Sua instalação do Minibolt Tools está completa, você dispões dos seguintes softwares, Balance of satoshis (usando: bos telegram no terminal, Thunderhub (porta 4002), lndg (porta 8889) e o lnbits (porta 4003)"
+}
+
+main () {
+  system_update
+  install_nodejs
+  install_bos
+  install_thunderhub
+  install_lndg
+  install_lnbits
+}
+
+main
