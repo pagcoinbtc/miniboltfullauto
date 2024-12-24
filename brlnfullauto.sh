7dea409fa7bb7d134c43004c25bd580f5bf81319
pagcoinbr <167659900+pagcoinbr@users.noreply.github.com>
2024-12-24 02:41:32 -0300

    Update brlnfullauto.sh

--- brlnfullauto.sh
+++ brlnfullauto.sh
@@ -7,9 +7,11 @@ TOR_GPGLINK=https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE
 LND_VERSION=0.18.3
 MAIN_DIR=/data
 LN_DDIR=/data/lnd
+LNDG_DIR=/home/admin/lndg
+VERSION_THUB=0.13.31
 
 update_and_upgrade() {
-  sudo apt update && sudo apt full-upgrade -y
+sudo apt update && sudo apt full-upgrade -y
 }
 
 create_main_dir() {
@@ -56,6 +58,10 @@ download_lnd() {
   sha256sum --check manifest-v$LND_VERSION-beta.txt --ignore-missing
   curl https://raw.githubusercontent.com/lightningnetwork/lnd/master/scripts/keys/roasbeef.asc | gpg --import
   gpg --verify manifest-roasbeef-v$LND_VERSION-beta.sig manifest-v$LND_VERSION-beta.txt
+  if [ $? -ne 0 ]; then
+    echo "####################################################################################### WARNING: GPG SIGNATURE NOT VERIFIED.##################################################################################################################################################"
+    exit 1
+  fi
   tar -xzf lnd-linux-amd64-v$LND_VERSION-beta.tar.gz
   sudo install -m 0755 -o root -g root -t /usr/local/bin lnd-linux-amd64-v$LND_VERSION-beta/lnd lnd-linux-amd64-v$LND_VERSION-beta/lncli
   sudo rm -r lnd-linux-amd64-v$LND_VERSION-beta lnd-linux-amd64-v$LND_VERSION-beta.tar.gz manifest-roasbeef-v$LND_VERSION-beta.sig manifest-roasbeef-v$LND_VERSION-beta.sig.ots manifest-v$LND_VERSION-beta.txt manifest-v$LND_VERSION-beta.txt.ots
@@ -69,102 +75,118 @@ configure_lnd() {
   sudo mkdir -p $LN_DDIR
   sudo chown -R admin:admin $LN_DDIR
   ln -s $LN_DDIR /home/admin/.lnd
-  echo "AVISO: Salve a senha que você escolher para a carteira Lightning. Caso contrário, você pode perder seus fundos. A senha deve ter pelo menos 8 caracteres."
-  while true; do
-    read -p "Escolha uma senha para a carteira Lightning: " password
-    echo
-    if [ ${#password} -ge 8 ]; then
-      break
-    else
-      echo "A senha deve ter pelo menos 8 caracteres. Tente novamente."
-    fi
-  done
-  echo "$password" > $LN_DDIR/password.txt
-  chmod 600 $LN_DDIR/password.txt
-  read -p "Digite o alias: " alias
-  read -p "Digite o bitcoind.rpcuser: " bitcoind_rpcuser
-  read -p "Digite o bitcoind.rpcpass: " bitcoind_rpcpass
   cat << EOF > $LN_DDIR/lnd.conf
-# BRLN: lnd configuration
-# /data/lnd/lnd.conf
+# MiniBolt: lnd configuration
+# /data/admin/lnd.conf
 
 [Application Options]
-alias=$alias | BR⚡LN
-debuglevel=info
-maxpendingchannels=3
+externalhosts=<ddns_clearnet>:9735
+# externalip=<fixed_ip_clearnet>
+# Up to 32 UTF-8 characters, accepts emojis i.e ⚡🧡​ https://emojikeyboard.top/
+alias=$alias|BR⚡️LN
+# You can choose the color you want at https://www.color-hex.com/
+color=#ff9900
 
-# Rest and gRPC API to LAN
-# Rest Externo para acesso Zeus Wallet
-restlisten=0.0.0.0:8080
-rpclisten=localhost:10009
-
-# Password: automatically unlock wallet with the password in this file
+# Automatically unlock wallet with the password in this file
 wallet-unlock-password-file=/data/lnd/password.txt
 wallet-unlock-allow-create=true
 
+# The TLS private key will be encrypted to the node's seed
+tlsencryptkey=true
+
 # Automatically regenerate certificate when near expiration
 tlsautorefresh=true
-# Do not include the interface IPs or the system hostname in TLS certificate.
+
+# Do not include the interface IPs or the system hostname in TLS certificate
 tlsdisableautofill=true
 
-#FOR CLEARNET USE ONLY - Uncomment and fill out with your address
-#listen=0.0.0.0:9740
-#externalhosts=myadress.ddns.net:9740
+## Channel settings
+# (Optional) Minimum channel size. Uncomment and set whatever you want
+# (default: 20000 sats)
+minchansize=1000000
 
-# Channel settings
-bitcoin.basefee=1000
-bitcoin.feerate=500
-minchansize=50000
-maxchansize=10000000
+## (Optional) High fee environment settings
+#max-commit-fee-rate-anchors=10
+#max-channel-fee-allocation=0.5
+
+## Communication
 accept-keysend=true
 accept-amp=true
-coop-close-target-confs=288
-bitcoin.defaultchanconfs=2
-bitcoin.timelockdelta=80
+
+## Rebalancing
 allow-circular-route=true
-bitcoin.defaultchanconfs=2
-max-cltv-expiry=2016
-max-commit-fee-rate-anchors=100
 
-routing.strictgraphpruning=true
-rpcmiddleware.enable=true
-routerrpc.minrtprob=0.001
-routerrpc.apriori.hopprob=0.7
-routerrpc.apriori.weight=0.3
-routerrpc.apriori.penaltyhalflife=2h
-routerrpc.attemptcost=10
-routerrpc.attemptcostppm=100
-routerrpc.maxmchistory=100000
-caches.channel-cache-size=100000
+## Modo Hibrido
+# specify an interface (IPv4/IPv6) and port (default 9735) to listen on
+# listen on IPv4 interface or listen=[::1]:9736 on IPv6 interface
+listen=0.0.0.0:9735
+# listen=[::1]:9736
 
-# Watchtower
-wtclient.active=true
-
-# Performance
-sync-freelist=false
+## Performance
 gc-canceled-invoices-on-startup=true
 gc-canceled-invoices-on-the-fly=true
-ignore-historical-gossip-filters=1
-stagger-initial-reconnect=true
-payments-expiration-grace-period=10000h
-
-# Database
-[bolt]
-db.bolt.auto-compact=false
-db.bolt.auto-compact-min-age=0h
+ignore-historical-gossip-filters=true
+restlisten=0.0.0.0:8080
+rpclisten=0.0.0.0:10009
 
 [Bitcoin]
 bitcoin.mainnet=true
 bitcoin.node=bitcoind
 
-[bitcoind]
+# Fee settings - default LND base fee = 1000 (mSat), fee rate = 1 (ppm)
+# You can choose whatever you want e.g ZeroFeeRouting (0,0) or ZeroBaseFee (0,X)
+bitcoin.basefee=0
+bitcoin.feerate=0
+# (Optional) Specify the CLTV delta we will subtract from a forwarded HTLC's timelock value
+# (default: 80)
+#bitcoin.timelockdelta=80
 
-#Bitcoin VPS
+#[Bitcoind]
+#bitcoind.rpchost=127.0.0.1:8332
+#bitcoind.rpcuser=
+#bitcoind.rpcpass=
+#bitcoind.zmqpubrawblock=tcp://127.0.0.1:28332
+#bitcoind.zmqpubrawtx=tcp://127.0.0.1:28333
+
+[Bitcoind]
 bitcoind.rpchost=bitcoin.br-ln.com:8085
 bitcoind.rpcuser=$bitcoind_rpcuser
 bitcoind.rpcpass=$bitcoind_rpcpass
 bitcoind.zmqpubrawblock=tcp://bitcoin.br-ln.com:28332
 bitcoind.zmqpubrawtx=tcp://bitcoin.br-ln.com:28333
+
+[protocol]
+protocol.wumbo-channels=false
+protocol.option-scid-alias=true
+protocol.simple-taproot-chans=true
+
+[wtclient]
+## Watchtower client settings
+wtclient.active=true
+
+# (Optional) Specify the fee rate with which justice transactions will be signed
+# (default: 10 sat/byte)
+#wtclient.sweep-fee-rate=10
+
+[watchtower]
+## Watchtower server settings
+watchtower.active=true
+
+[routing]
+routing.strictgraphpruning=true
+
+[bolt]
+## Database
+# Set the next value to false to disable auto-compact DB
+# and fast boot and comment the next line
+db.bolt.auto-compact=true
+# Uncomment to do DB compact at every LND reboot (default: 168h)
+#db.bolt.auto-compact-min-age=0h
+
+## High fee environment (Optional)
+# (default: CONSERVATIVE) Uncomment the next 2 lines
+#[Bitcoind]
+#bitcoind.estimatemode=ECONOMICAL
 
 [tor]
 # If clearnet active, change the 2 lines below to true and false
@@ -222,15 +244,39 @@ EOF'
   sudo chmod -R g+X $LN_DDIR
   sudo chmod 640 /run/tor/control.authcookie
   sudo chmod 750 /run/tor
+    while true; do
+    read -p "Escolha uma senha para a carteira Lightning: " password
+    echo
+    if [ ${#password} -ge 8 ]; then
+      break
+    else
+      echo "A senha deve ter pelo menos 8 caracteres. Tente novamente."
+    fi
+  done
+  echo "$password" > $LN_DDIR/password.txt
+  chmod 600 $LN_DDIR/password.txt
   sudo systemctl enable lnd
   sudo systemctl start lnd
-  echo "Execute o comando:" 
-  echo "lncli create"
-  echo "Depois, digite a senha 2x para confirmar e pressione 'n' para criar uma nova cateira, digite o "password" e pressione *enter* para criar uma nova carteira."
+  sleep 10
+  echo "###############################################################################################"
+  echo "Agora Você irá criar sua senha, digite a senha 3x para confirmar e pressione 'n' para criar uma nova cateira ou "y" para recuperar uma carteira antiga com 24 palavras, digite o "password" caso queira proteger sua frade de 24 palavras com uma senha e pressione *enter* para criar uma nova carteira."
+  echo "AVISO!: Anote sua frase de 24 palavras com ATENÇÃO, AGORA! Esta frase não pode ser recuperada se não anotada agora. Caso contrário, você pode perder seus fundos. A senha deve ter pelo menos 8 caracteres."
+  echo "###############################################################################################"
+  lncli --tlscertpath /data/lnd/tls.cert.tmp create
+  while true; do
+    read -p "Digite 'yes' para continuar a instalação do seu nó lightning após anotar a frase de 24 palavras: " confirm
+    case $confirm in
+      [Yy][Ee][Ss])
+        break
+        ;;
+      *)
+        echo "Por favor, digite 'yes' para continuar."
+        ;;
+    esac
+  done
   }
 
 install_bitcoind() {
-    read -p "Escolha sua senha do Bitcoin Core: " rpcpsswd
     cd /tmp
     VERSION=28.0
     wget https://bitcoincore.org/bin/bitcoin-core-$VERSION/bitcoin-$VERSION-x86_64-linux-gnu.tar.gz
@@ -361,56 +407,308 @@ sudo ss -tulpn | grep bitcoind
 echo "Bitcoind instalado com sucesso!"
 }
 
-install_lnd() {
-    download_lnd
-  configure_lnd
-  create_lnd_service
-  echo "LND Instalado!"
+install_nodejs() {
+  curl -sL https://deb.nodesource.com/setup_21.x | sudo -E bash -
+  sudo apt-get install nodejs -y
+  
 }
 
-system_preparation() {
-  update_and_upgrade
-  create_main_dir
-  configure_ufw
-  install_tor
+install_bos() {
+  mkdir -p ~/.npm-global
+  npm config set prefix '~/.npm-global'
+if ! grep -q 'PATH="$HOME/.npm-global/bin:$PATH"' ~/.profile; then
+  echo 'PATH="$HOME/.npm-global/bin:$PATH"' >> ~/.profile
+fi
+  source ~/.profile
+  npm i -g balanceofsatoshis
+  bos --version
+  sudo bash -c 'echo "127.0.0.1" >> /etc/hosts'
+  sudo chown -R $USER:$USER /data/lnd
+  sudo chmod -R 755 /data/lnd
+  export BOS_DEFAULT_LND_PATH=/data/lnd
+  mkdir -p ~/.bos/$alias
+  base64 -w0 /data/lnd/tls.cert > /data/lnd/tls.cert.base64
+  base64 -w0 /data/lnd/data/chain/bitcoin/mainnet/admin.macaroon > /data/lnd/data/chain/bitcoin/mainnet/admin.macaroon.base64
+  cert_base64=$(cat /data/lnd/tls.cert.base64)
+  macaroon_base64=$(cat /data/lnd/data/chain/bitcoin/mainnet/admin.macaroon.base64)
+  bash -c "cat <<EOF > ~/.bos/$alias/credentials.json
+{
+  "cert": "$cert_base64",
+  "macaroon": "$macaroon_base64",
+  "socket": "localhost:10009"
+}
+EOF"
+  sudo bash -c "cat <<EOF > /etc/systemd/system/bos-telegram.service
+# Systemd unit for Bos-Telegram Bot
+# /etc/systemd/system/bos-telegram.service
+# Substitua as variáveis iniciadas com \$ com suas informações
+# Não esquece de apagar o \$
+
+[Unit]
+Description=bos-telegram
+Wants=lnd.service
+After=lnd.service
+
+[Service]
+ExecStart=/home/admin/.npm-global/bin/bos telegram --use-small-units --connect <seu_connect_code_aqui>
+User=admin
+Restart=always
+TimeoutSec=120
+RestartSec=30
+StandardOutput=null
+StandardError=journal
+Environment=BOS_DEFAULT_LND_PATH=/data/lnd
+
+[Install]
+WantedBy=multi-user.target
+EOF"
+  sudo systemctl daemon-reload
+  sudo systemctl start bos-telegram.service
+  sudo systemctl enable bos-telegram.service
+}
+
+install_thunderhub() {
+  node -v
+  npm -v
+  sudo apt update && sudo apt full-upgrade -y
+  cd
+  curl https://github.com/apotdevin.gpg | gpg --import
+  git clone --branch v$VERSION_THUB https://github.com/apotdevin/thunderhub.git && cd thunderhub
+  git verify-commit v$VERSION_THUB
+  sudo apt update && sudo apt full-upgrade -y
+  npm install
+  npm run build
+sudo ufw allow 3000/tcp comment 'allow ThunderHub SSL from anywhere'
+cp .env .env.local
+sed -i '51s|.*|ACCOUNT_CONFIG_PATH="/home/admin/thunderhub/thubConfig.yaml"|' .env.local
+bash -c "cat <<EOF > thubConfig.yaml
+masterPassword: 'PASSWORD'
+accounts:
+  - name: 'MiniBolt'
+    serverUrl: '127.0.0.1:10009'
+    macaroonPath: '/data/lnd/data/chain/bitcoin/mainnet/admin.macaroon'
+    certificatePath: '/data/lnd/tls.cert'
+    password: '[E] ThunderHub password'
+EOF"
+sed -i "7s|\[E\] ThunderHub password|$senha|" thubConfig.yaml
+sudo bash -c 'cat <<EOF > /etc/systemd/system/thunderhub.service
+# MiniBolt: systemd unit for Thunderhub
+# /etc/systemd/system/thunderhub.service
+
+[Unit]
+Description=ThunderHub
+Requires=lnd.service
+After=lnd.service
+
+[Service]
+WorkingDirectory=/home/admin/thunderhub
+ExecStart=/usr/bin/npm run start
+
+User=admin
+
+# Process management
+####################
+TimeoutSec=300
+
+# Hardening Measures
+####################
+PrivateTmp=true
+ProtectSystem=full
+NoNewPrivileges=true
+PrivateDevices=true
+
+[Install]
+WantedBy=multi-user.target
+EOF'
+sudo systemctl start thunderhub.service
+sudo systemctl enable thunderhub.service
+}
+
+install_lndg () {
+sudo ufw allow 8889/tcp comment 'allow lndg SSL from anywhere'
+cd
+git clone https://github.com/cryptosharks131/lndg.git
+cd lndg
+sudo apt install -y virtualenv
+virtualenv -p python3 .venv
+.venv/bin/pip install -r requirements.txt
+.venv/bin/pip install whitenoise
+.venv/bin/python initialize.py --whitenoise
+sudo tee /etc/systemd/system/lndg-controller.service > /dev/null <<EOF
+[Unit]
+Description=Controlador de backend para Lndg
+
+[Service]
+Environment=PYTHONUNBUFFERED=1
+User=admin
+Group=admin
+ExecStart=$LNDG_DIR/.venv/bin/python $LNDG_DIR/controller.py
+StandardOutput=append:/var/log/lndg-controller.log
+StandardError=append:/var/log/lndg-controller.log
+Restart=always
+RestartSec=60s
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo tee /etc/systemd/system/lndg.service > /dev/null  <<EOF
+[Unit]
+Description=LNDG Django Server
+After=network.target
+
+[Service]
+Environment=PYTHONUNBUFFERED=1
+User=admin
+Group=admin
+WorkingDirectory=$LNDG_DIR
+ExecStart=$LNDG_DIR/.venv/bin/python $LNDG_DIR/manage.py runserver 0.0.0.0:8889
+StandardOutput=append:/var/log/lndg.log
+StandardError=append:/var/log/lndg.log
+Restart=always
+RestartSec=5
+TimeoutSec=300
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo systemctl daemon-reload
+sudo systemctl enable lndg-controller.service
+sudo systemctl start lndg-controller.service
+sudo systemctl enable lndg.service
+sudo systemctl start lndg.service
+}
+
+manage_bitcoin_node() {
+    # Caminho do arquivo de configuração e arquivos para apagar
+    local LND_CONF="/home/admin/.lnd/lnd.conf"
+    local FILES_TO_DELETE=(
+        "/home/admin/.lnd/tls.cert"
+        "/home/admin/.lnd/tls.key"
+        "/home/admin/.lnd/v3_onion_private_key"
+    )
+
+    # Função interna para comentar linhas 73 a 78
+    comment_lines() {
+        sed -i '73,78 s/^/#/' "$LND_CONF"
+    }
+
+    # Função interna para descomentar linhas 73 a 78
+    decomment_lines() {
+        sed -i '73,78 s/^#//' "$LND_CONF"
+    }
+
+    # Função interna para apagar os arquivos
+    delete_files() {
+        for file in "${FILES_TO_DELETE[@]}"; do
+            if [ -f "$file" ]; then
+                rm -f "$file"
+                echo "Deleted: $file"
+            else
+                echo "File not found: $file"
+            fi
+        done
+    }
+
+    # Função interna para reiniciar o serviço LND
+    restart_lnd() {
+        sudo systemctl restart lnd
+        if [ $? -eq 0 ]; then
+            echo "LND service restarted successfully."
+        else
+            echo "Failed to restart LND service."
+        fi
+    }
+
+    # Exibir o menu para o usuário
+    while true; do
+        echo "Escolha uma opção:"
+        echo "1) Trocar para o Bitcoin Core local"
+        echo "2) Trocar para o node Bitcoin remoto"
+        echo "3) Sair"
+        read -p "Digite sua escolha: " choice
+
+        case $choice in
+            1)
+                echo "Trocando para o Bitcoin Core local..."
+                comment_lines
+                delete_files
+                restart_lnd
+                echo "Trocado para o Bitcoin Core local."
+                ;;
+            2)
+                echo "Trocando para o node Bitcoin remoto..."
+                decomment_lines
+                delete_files
+                restart_lnd
+                echo "Trocado para o node Bitcoin remoto."
+                ;;
+            3)
+                echo "Saindo."
+                break
+                ;;
+            *)
+                echo "Escolha inválida. Por favor, tente novamente."
+                ;;
+        esac
+        echo ""
+    done
+}
+
+main() {
+read -p "Digite a senha para ThunderHub: " senha
+read -p "Digite seu alias(nome do nó): " alias
+read -p "Digite o bitcoind.rpcuser(BRLN): " bitcoind_rpcuser
+read -p "Digite o bitcoind.rpcpass(BRLN): " bitcoind_rpcpass
+read -p "Escolha sua senha do Bitcoin Core: " rpcpsswd
+    update_and_upgrade
+    create_main_dir
+    configure_ufw
+    install_tor
+    download_lnd
+    configure_lnd
+    create_lnd_service
+    install_nodejs
+    install_bos
+    install_thunderhub
+    install_lndg
+    install_bitcoind
 }
 
 menu() {
-  echo "##############################################"
-  echo "@  Siga os passos, fazendo a instalação na   @"
-  echo "@          odem sugerida 1, 2 e 3.           @"
-  echo "##############################################"
-  echo "Escolha uma opção:"
-  echo "1) Preparação de Rede Privada (Tor + Ip2)."
-  echo "2) Instalar Lightning Node."
-  echo "3) Instalar Bitcoin Node."
-  echo "0) Sair"
-  read -p "Opção: " option
+  echo "🌟 Bem-vindo à instalação de node Lightning personalizado da BRLN! 🌟"
+  echo
+  echo "⚡ Este script instalará:"
+  echo "  🛠️ Nó Lightning Standalone"
+  echo "  🏗️ Bitcoin Core"
+  echo "  🖥️ Ferramentas de administração:"
+  echo "    - ThunderHub"
+  echo "    - Balance of Satoshis (BOS)"
+  echo "    - LNDG"
+  echo
+  echo "📝 Escolha uma opção:"
+  echo "   1- Instalação do BRLN Bolt"
+  echo "   2- Alterne Bitcoin Local/Remoto"
+  echo "   0- Sair"
+  echo
+  read -p "👉 Digite sua escolha: " option
 
   case $option in
     1)
-      system_preparation
-      echo "Instale o Lightning Node (2)"
-      menu
+      echo "🚀 Iniciando a instalação..."
+      main
       ;;
     2)
-      install_lnd
-      echo "instale o Bitcoin Node (3)"
-      menu
-      ;;
-    3)
-      install_bitcoind
-      echo "Término da instalação"
-      exit 0
+      manage_bitcoin_node
       ;;
     0)
-      echo "Saindo..."
+      echo "👋 Saindo... Até a próxima!"
       exit 0
       ;;
     *)
-      echo "Opção inválida!"
+      echo "❌ Opção inválida! Tente novamente."
       ;;
   esac
 }
+
 
 menu
